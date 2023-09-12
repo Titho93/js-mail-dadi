84b1a60a86b5fba0d097149cba93c7ac774a082d
Titho93 <titho10.93@gmail.com>
2023-09-12 15:12:41 +0200

    [add]const mail

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Es Mail 
+
+const listMail = ['rosso@gmail.com','blu@gmail.com','verde@gmail.com','giallo@gmail.com']
+const mail = listMail.join(',')
+const yourMail = (prompt('inserisci la tua Email'))
+
