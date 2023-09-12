f7b61c9c6bc275eac9c836cf4de0a965c070a227
Titho93 <titho10.93@gmail.com>
2023-09-12 15:32:59 +0200

    [add]if mail

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // Es Mail 
 
-const listMail = ['rosso@gmail.com','blu@gmail.com','verde@gmail.com','giallo@gmail.com']
-const mail = listMail.join(',')
-const yourMail = (prompt('inserisci la tua Email'))
+const listMail = ['rosso@gmail.com','blu@gmail.com','verde@gmail.com','giallo@gmail.com'];
+const mail = listMail.join(' - ');
+const yourMail = (prompt('inserisci la tua Email'));
+let messaggio;
 
+if(yourMail === mail){
+  messaggio = 'Password corretta';
+}else {
+  messaggio = 'Password errata';
+}
+console.log(messaggio)
+
+document.getElementById('output').innerHTML = messaggio;
