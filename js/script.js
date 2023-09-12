07dde068005556442c1cf5391f053c04161ed573
Titho93 <titho10.93@gmail.com>
2023-09-12 16:35:31 +0200

    [fix]mail bug

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 // Es Mail 
 
 const listMail = ['rosso@gmail.com','blu@gmail.com','verde@gmail.com','giallo@gmail.com'];
-const mail = listMail.join(' - ');
 const yourMail = (prompt('inserisci la tua Email'));
 let messaggio;
 
-if(yourMail === mail){
-  messaggio = 'Password corretta';
-}else {
-  messaggio = 'Password errata';
+
+for(let i = 0; i < listMail.length; i++){
+const mail = listMail[i]
+console.log(mail)
+
+  if (yourMail === mail){
+    messaggio = 'Mail corretta';
+  }else if(!yourMail === mail) {
+    messaggio = 'Mail errata';
+  }
+  console.log(messaggio)
+  
 }
-console.log(messaggio)
+
+
 
 document.getElementById('output').innerHTML = messaggio;
